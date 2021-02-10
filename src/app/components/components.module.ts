784efe61461b319d2056db2b83333768989efd6c
Milagros Alfaro <milafrontend@gmail.com>
2021-02-10 18:24:31 -0500

    componentes nuevos

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MancolistaComponent } from './mancolista/mancolista.component';
+import { ConfiguracionComponent } from './configuracion/configuracion.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, MancolistaComponent],
+  declarations: [HeaderComponent, FooterComponent, MancolistaComponent, ConfiguracionComponent],
   imports: [
     CommonModule
   ],
   exports : [
     HeaderComponent,
     FooterComponent,
-    MancolistaComponent
+    MancolistaComponent,
+    ConfiguracionComponent
   ],
 
 })
