041b6bb1da13fe9e390cba2660033bd5ffc0e78e
Bryan Andy <bryanandy0419@gmail.com>
2021-02-14 13:00:44 -0500

    add subasta tienda

--- src/app/user/components/user-components.module.ts
+++ src/app/user/components/user-components.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfiguracionComponent } from './configuracion/configuracion.component';
 import { MancolistaComponent } from './mancolista/mancolista.component';
+import { RouterModule } from '@angular/router';
+import { SubastaComponent } from './subasta/subasta.component';
+import { TiendauserComponent } from './tiendauser/tiendauser.component';
 
 
 
 @NgModule({
   declarations: [
      ConfiguracionComponent,
-     MancolistaComponent
+     MancolistaComponent,
+     SubastaComponent,
+     TiendauserComponent
   ],
   exports : [
     ConfiguracionComponent,
     MancolistaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class UserComponentsModule { }
--- src/app/user/user.routing.ts
+++ src/app/user/user.routing.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
 import { MancolistaComponent } from './components/mancolista/mancolista.component';
 import { DashboardUserComponent } from './dashboard-user/dashboard-user.component';
+import { SubastaComponent } from './components/subasta/subasta.component';
+import { TiendauserComponent } from './components/tiendauser/tiendauser.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardUserComponent,
     children: [
-        { path: '', component: DashboardUserComponent },
+        { path: '', component: SubastaComponent },
+        { path: 'subasta', component: SubastaComponent },
         { path: 'configuracion', component: ConfiguracionComponent },
         { path: 'mancolista', component: MancolistaComponent },
+        { path: 'tienda', component: TiendauserComponent },
     ]
   }
 ];
